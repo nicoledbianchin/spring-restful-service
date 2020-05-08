2c6ecad570b1dc14512704111ed9d3d4ee48e136
nicoledbianchin <nicoledbianchin@gmail.com>
2020-05-08 00:05:30 -0300

    initial commit

---
+++ src/main/java/com/nicole/springrestfulservice/SpringRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nicole.springrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicole/springrestfulservice/SpringRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicole.springrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
