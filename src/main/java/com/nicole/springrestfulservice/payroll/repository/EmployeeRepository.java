9b02a32cb9758c0b12f8047443e37a433a8422aa
nicoledbianchin <nicoledbianchin@gmail.com>
2020-05-19 07:56:19 -0300

    Revert "zdfdf"
    This reverts commit bbc538c3198d195f72d915593bbf2286ef009fc0.

--- src/main/java/com/nicole/springrestfulservice/payroll/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nicole.springrestfulservice.payroll.repository;
-
-import com.nicole.springrestfulservice.payroll.models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
