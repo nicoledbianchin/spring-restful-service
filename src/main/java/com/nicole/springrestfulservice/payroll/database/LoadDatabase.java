517a6ab2a297057e86fa3642b9aa316c788b3fbd
nicoledbianchin <nicoledbianchin@gmail.com>
2020-06-09 20:14:54 -0300

    update load database

--- src/main/java/com/nicole/springrestfulservice/payroll/database/LoadDatabase.java
+++ src/main/java/com/nicole/springrestfulservice/payroll/database/LoadDatabase.java
@@ -1,23 +1,33 @@
 package com.nicole.springrestfulservice.payroll.database;
 
 import com.nicole.springrestfulservice.payroll.models.Employee;
+import com.nicole.springrestfulservice.payroll.models.Order;
+import com.nicole.springrestfulservice.payroll.models.Status;
 import com.nicole.springrestfulservice.payroll.repository.EmployeeRepository;
-
-import lombok.extern.slf4j.Slf4j;
-
+import com.nicole.springrestfulservice.payroll.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Slf4j
 public class LoadDatabase {
 
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "bulgar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+            employeeRepository.save(new Employee("Bilbo", "Baggins", "bulgar"));
+            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preload " + employee));
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> log.info("Preload " + order));
         };
     }
 }
