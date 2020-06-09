18decc0d0f446287843c82878d8be771ab1f54a1
nicoledbianchin <nicoledbianchin@gmail.com>
2020-06-09 20:08:08 -0300

    add complete to order controller

--- src/main/java/com/nicole/springrestfulservice/payroll/controller/OrderController.java
+++ src/main/java/com/nicole/springrestfulservice/payroll/controller/OrderController.java
@@ -71,4 +71,21 @@ public class OrderController {
                         .withTitle("Method not allowed") //
                         .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status"));
     }
+
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id) {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
+
+        if(order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                    .withTitle("Method not allowed")
+                    .withBody("You can'' complete and order that is in the " + order.getStatus() + " status"));
+    }
 }
